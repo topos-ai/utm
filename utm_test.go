0c4eaccbe4902056ab56343143ade445ee115e6d
Eric Buth <eric@topos.com>
2019-10-24 15:51:29 -0400

    project test

--- utm_test.go
+++ utm_test.go
@@ -33,8 +33,6 @@ func TestRadians(t *testing.T) {
 
 	for _, test := range tests {
 		latitude, longitude := Radians(test.zone, test.south, test.easting, test.northing)
-		easting, northing := Project(test.zone, test.south, latitude, longitude)
-
 		latitude *= 180 / math.Pi
 		if latitude-test.latitude >= testDegreePrecision {
 			t.Errorf("expected latitude %f, got %f", test.latitude, latitude)
@@ -44,12 +42,37 @@ func TestRadians(t *testing.T) {
 		if longitude-test.longitude >= testDegreePrecision {
 			t.Errorf("expected longitude %f, got %f", test.longitude, longitude)
 		}
+	}
+}
 
+func TestProject(t *testing.T) {
+	tests := []struct {
+		zone      int
+		south     bool
+		easting   float64
+		northing  float64
+		latitude  float64
+		longitude float64
+	}{
+		// echo -74.044439711 40.689170987 | cs2cs +proj=latlong +to +proj=utm +zone=18 +ellps=WGS84 -d 9
+		{18, false, 580740.999972106, 4504692.000000370, 40.689170987, -74.044439711}, // Statue of Liberty
+		// echo 78.041942295 27.175000872 | cs2cs +proj=latlong +to +proj=utm +zone=44 +ellps=WGS84 -d 9
+		{44, false, 206895.000000704, 3009275.999957789, 27.175000872, 78.041942295}, // Taj Mahal
+		// echo 151.214164458 33.858611864 | cs2cs +proj=latlong +to +proj=utm +zone=56 +south +ellps=WGS84 -d 9
+		{56, true, 334798.999960904, 13747914.000003856, -33.858611864, 151.214164458}, // Sydney Opera House
+		// echo -43.210553081 -22.951948534 | cs2cs +proj=latlong +to +proj=utm +zone=23 +south +ellps=WGS84 -d 9
+		{23, true, 683470.999988894, 7460682.000030654, -22.951948534, -43.210553081}, // Christ the Redeemer
+		// echo 166.668248472 -77.846321004 | cs2cs +proj=latlong +to +proj=utm +zone=58 +south +ellps=WGS84 -d 9
+		{58, true, 539203.000000174, 1358222.999990588, -77.846321004, 166.668248472}, // McMurdo Station
+	}
+
+	for _, test := range tests {
+		easting, northing := Project(test.zone, test.south, test.latitude*math.Pi/180, test.longitude*math.Pi/180)
 		if easting-test.easting >= testProjectionPrecision {
 			t.Errorf("expected easting %f, got %f", test.easting, easting)
 		}
 
-		if easting-test.northing >= testProjectionPrecision {
+		if northing-test.northing >= testProjectionPrecision {
 			t.Errorf("expected northing %f, got %f", test.northing, northing)
 		}
 	}
